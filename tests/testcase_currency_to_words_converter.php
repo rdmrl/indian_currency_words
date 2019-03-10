65a40d71a5341a29c82124edb348093377c863ac
Ravi Damarla <ravi.damarla@gmail.com>
2019-03-10 15:00:54 +0000

    formatted

--- tests/testcase_currency_to_words_converter.php
+++ tests/testcase_currency_to_words_converter.php
@@ -6,83 +6,83 @@ require_once __DIR__ . '/../library/class_currency_to_words_converter.php';
 
 class CurrencyToWordsConverterTest extends TestCase 
 {
-	function testConvert1() 
+  function testConvert1() 
   {
     $expected1 = "One Crore, Thirty-eight Lakh, Three Thousand and Thirty rupees and One paisa only";
 
-		$test_value1 = 13803030.01;
-		$result1 = CurrencyToWordsConverter::convertToWords( $test_value1 );
+    $test_value1 = 13803030.01;
+    $result1 = CurrencyToWordsConverter::convertToWords( $test_value1 );
 
     $this->assertEquals( $result1, $expected1 );
   }
 
-	function testConvertPaiseOnly() 
+  function testConvertPaiseOnly() 
   {
     $expected = "Twenty-three paise only";
 
     $test_value = ".23";
-		$result = CurrencyToWordsConverter::convertToWords( $test_value );
+    $result = CurrencyToWordsConverter::convertToWords( $test_value );
 
     $this->assertEquals( $result, $expected );
-	}
+  }
 
-	function testConvertUnits() 
+  function testConvertUnits() 
   {
     $expected = "One rupee and Twenty-three paise only";
 
     $test_value = "1.23";
-		$result = CurrencyToWordsConverter::convertToWords( $test_value );
+    $result = CurrencyToWordsConverter::convertToWords( $test_value );
 
     $this->assertEquals( $result, $expected );
-	}
+  }
 
-	function testConvertTens() 
+  function testConvertTens() 
   {
     $expected = "Thirty rupees and Twenty-three paise only";
 
     $test_value = "30.23";
-		$result = CurrencyToWordsConverter::convertToWords( $test_value );
+    $result = CurrencyToWordsConverter::convertToWords( $test_value );
 
     $this->assertEquals( $result, $expected );
-	}
+  }
 
-	function testConvertHundreds() 
+  function testConvertHundreds() 
   {
     $expected = "Four Hundred and Sixty-four rupees and Twenty-three paise only";
 
     $test_value = "464.23";
-		$result = CurrencyToWordsConverter::convertToWords( $test_value );
+    $result = CurrencyToWordsConverter::convertToWords( $test_value );
 
     $this->assertEquals( $result, $expected );
-	}
+  }
 
-	function testConvertThousands() 
+  function testConvertThousands() 
   {
     $expected = "Seven Thousand, Nine Hundred and Eighty-three rupees and Twenty-three paise only";
 
     $test_value = "7983.23";
-		$result = CurrencyToWordsConverter::convertToWords( $test_value );
+    $result = CurrencyToWordsConverter::convertToWords( $test_value );
 
     $this->assertEquals( $result, $expected );
-	}
+  }
 
-	function testConvertSeveralThousands() 
+  function testConvertSeveralThousands() 
   {
     $expected = "Sixty-seven Thousand, Nine Hundred and Eighty-three rupees and Twenty-three paise only";
 
     $test_value = "67983.23";
-		$result = CurrencyToWordsConverter::convertToWords( $test_value );
+    $result = CurrencyToWordsConverter::convertToWords( $test_value );
 
     $this->assertEquals( $result, $expected );
-	}
+  }
 
-	function testConvertSeveralThousandsWithoutAnd() 
+  function testConvertSeveralThousandsWithoutAnd() 
   {
     $expected = "Sixty-seven Thousand, Nine Hundred, Eighty-three rupees, Twenty-three paise only";
 
     $test_value = "67983.23";
-		$result = CurrencyToWordsConverter::convertToWords( $test_value, array( 'hundreds_separator' => ', ' ) );
+    $result = CurrencyToWordsConverter::convertToWords( $test_value, array( 'hundreds_separator' => ', ' ) );
 
     $this->assertEquals( $result, $expected );
-	}
+  }
 }
